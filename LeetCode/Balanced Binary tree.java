ef9b20fb4558c9aaaba44a80b8c2bc5dbbbe927d
shahnupur1901 <67814929+shahnupur1901@users.noreply.github.com>
2021-03-30 22:37:20 +0530

    Create Balanced Binary tree.java

---
+++ LeetCode/Balanced Binary tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) return true;
+        
+        return Math.abs(height(root.left) - height(root.right)) <=1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+    int height(TreeNode root){
+        if(root==null) return 0;
+        int l = height(root.left);
+        int r = height(root.right);
+        return l >  r ?  l+1: r+1;
+        
+    }
+}
